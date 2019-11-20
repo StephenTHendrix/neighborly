b57730cbbbe3a11797d875e6fafc28aa20753a28
Lindsay <lindsay.lbm@gmail.com>
2019-11-20 10:22:51 -0600

    created pages

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,31 @@
-import React from "react";
+import React, { Component } from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+// Import Pages ==========================================================================================
+import Welcome from "./pages/Welcome";
+import VolunteerSignUp from "./pages/Volunteer/VolunteerSignUp"
+import VolunteerDashboard from "./pages/Volunteer/VolunteerDashboard"
+import VolunteerProfile from "./pages/Volunteer/VolunteerProfile"
+import SeekerSignUp from "./pages/Seeker/SeekerSignUp"
+import SeekerDashboard from "./pages/Seeker/SeekerDashboard"
+import SeekerDashboard from "./pages/Seeker/SeekerProfile"
+//========================================================================================================
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Starter</h1>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <Router>
+        <Switch>
+          <Route exact path="/" component={Welcome} />
+          <Route exact path="/volunteer/signup" component={VolunteerSignUp} />
+          <Route exact path="/volunteer" component={VolunteerDashboard} />
+          <Route exact path="/volunteer/profile" component={VolunteerProfile} />
+          <Route exact path="/seeker/signup" component={SeekerSignUp} />
+          <Route exact path="/seeker" component={SeekerDashboard} />
+          <Route exact path="/seeker/profile" component={SeekerProfile} />
+        </Switch>
+      </Router>
+    )
+  }
 }
 
 export default App;
