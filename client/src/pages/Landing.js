279b133aaff9727665395b7d3660ecb296502d0d
Adam Norton <adam@adamnorton.com>
2019-11-21 10:53:10 -0600

    setting up the css and tags to fix font issues as well as adding new font options.

--- client/src/pages/Landing.js
+++ client/src/pages/Landing.js
@@ -6,7 +6,7 @@ class Landing extends Component {
       
       <div>
         <div className="vimeo-wrapper">
-          <iframe src="https://player.vimeo.com/video/374576663?background=1&autoplay=1&loop=1&title=0&byline=0&portrait=0"  frameborder="0" allow="autoplay; fullscreen" allowfullscreen></iframe>
+          <iframe title="spinning earth" src="https://player.vimeo.com/video/374576663?background=1&autoplay=1&loop=1&title=0&byline=0&portrait=0"  frameborder="0" allow="autoplay; fullscreen" allowfullscreen></iframe>
         </div> 
         <script src="https://player.vimeo.com/api/player.js"></script>
 
@@ -21,18 +21,18 @@ class Landing extends Component {
             <div className="col-xs-1 col-sm-3 img-boxes p-0" id="img-box1">
               <p className="text-upper">Local</p>
               <p className="text-lower">Community</p>
-              <img className="img-fluid img-hero" src="./assets/images/local.jpg" />
+              <img className="img-fluid img-hero" src="./assets/images/local.jpg" alt="volunteer with puppy" />
               
             </div>
             <div className="col-xs-1 col-sm-3 img-boxes p-0" id="img-box2">
               <p className="text-upper">Global</p>
               <p className="text-lower">Outreach</p>
-              <img className="img-fluid img-hero" src="./assets/images/global.jpg" />
+              <img className="img-fluid img-hero" src="./assets/images/global.jpg" alt="volunteer with child" />
             </div>
             <div className="col-xs-1 col-sm-3 img-boxes p-0" id="img-box3">
               <p className="text-upper">Environmental</p>
               <p className="text-lower">Conservation</p>
-              <img className="img-fluid img-hero" src="./assets/images/cleanup.jpg" />
+              <img className="img-fluid img-hero" src="./assets/images/cleanup.jpg" alt="clean up of oil spill" />
             </div>
           </div>
         </div>
