e3ee005dc76f60b50240d675b6eb1196e1e66c30
Adam Norton <adam@adamnorton.com>
2019-11-20 22:18:39 -0600

    got video background playing and streaming off of Vimeo for performance and bandwidth reasons.

--- client/src/pages/Landing.js
+++ client/src/pages/Landing.js
@@ -3,14 +3,14 @@ import React, { Component } from 'react'
 class Landing extends Component {
   render() {
     return (
-      <div className="container">
-        <div className="jumbotron mt-5">
-          <div className="col-sm-8 mx-auto">
-
-            <h1 className="text-center title">Neighborly</h1>
-          </div>
-        </div>
+      
+      <div>
+        <div className="vimeo-wrapper">
+          <iframe src="https://player.vimeo.com/video/374576663?background=1&autoplay=1&loop=1&title=0&byline=0&portrait=0"  frameborder="0" allow="autoplay; fullscreen" allowfullscreen></iframe>
+        </div> 
+        <script src="https://player.vimeo.com/api/player.js"></script>
       </div>
+      
     )
   }
 }
