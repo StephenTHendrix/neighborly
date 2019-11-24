6b63f929287911ad1dd17d89b7f68903784c327e
Lindsay <lindsay.lbm@gmail.com>
2019-11-24 13:44:55 -0600

    styling on volunteer register page. DOB need to delete slashes. Neeed to upload image. Need validations on password and email. Need to delete rogue submit button at bottom

--- client/src/components/Register.js
+++ client/src/components/Register.js
@@ -5,66 +5,68 @@ class Register extends Component {
 
   render(props) {
     return (
+      // <div className="row">
+      //   <div className="col-md-6 mt-5 mx-auto">
 
-      <div className="container">
-        <div className="row">
-          <div className="col-md-6 mt-5 mx-auto">
-            <form noValidate>
-              <h1 className="h3 mb-3 font-weight-normal title">Register</h1>
-              <div className="form-group">
-                <label htmlFor="name">First name</label>
-                <input
-                  type="text"
-                  className="form-control"
-                  name="first_name"
-                  placeholder="Enter your first name"
-                  value={this.props.first_name}
-                  onChange={this.props.onChange}
-                />
-              </div>
-              <div className="form-group">
-                <label htmlFor="name">Last name</label>
-                <input
-                  type="text"
-                  className="form-control"
-                  name="last_name"
-                  placeholder="Enter your last name"
-                  value={this.props.last_name}
-                  onChange={this.props.onChange}
-                />
-              </div>
-              <div className="form-group">
-                <label htmlFor="email">Email address</label>
-                <input
-                  type="email"
-                  className="form-control"
-                  name="email"
-                  placeholder="Enter email"
-                  value={this.props.email}
-                  onChange={this.props.onChange}
-                />
-              </div>
-              <div className="form-group">
-                <label htmlFor="password">Password</label>
-                <input
-                  type="password"
-                  className="form-control"
-                  name="password"
-                  placeholder="Password"
-                  value={this.props.password}
-                  onChange={this.props.onChange}
-                />
-              </div>
-              {/* <button
+      // <div className="container">
+      <form>
+        <h1 className="h3 mb-3 font-weight-normal text-center title">Register</h1>
+        <div className="form-row">
+          <div className="form-group col-md-6">
+            <label htmlFor="name">First name</label>
+            <input
+              type="text"
+              className="form-control"
+              name="first_name"
+              placeholder="Enter your first name"
+              value={this.props.first_name}
+              onChange={this.props.onChange}
+            />
+          </div>
+          <div className="form-group col-md-6">
+            <label htmlFor="name">Last name</label>
+            <input
+              type="text"
+              className="form-control"
+              name="last_name"
+              placeholder="Enter your last name"
+              value={this.props.last_name}
+              onChange={this.props.onChange}
+            />
+          </div>
+        </div>
+        <div className="form-group">
+          <label htmlFor="email">Email address</label>
+          <input
+            type="email"
+            className="form-control"
+            name="email"
+            placeholder="Email"
+            value={this.props.email}
+            onChange={this.props.onChange}
+          />
+        </div>
+        <div className="form-group">
+          <label htmlFor="password">Password</label>
+          <input
+            type="password"
+            className="form-control"
+            name="password"
+            placeholder="Password"
+            value={this.props.password}
+            onChange={this.props.onChange}
+          />
+        </div>
+        {/* <button
                 type="submit"
                 className="btn btn-lg btn-primary btn-block"
               >
                 Register!
               </button> */}
-            </form>
-          </div>
-        </div>
-      </div>
+      </form>
+      // </div>
+      // </div >
+      // </div >
     )
   }
 }
--- client/src/pages/Volunteer/VolunteerSignUp.js
+++ client/src/pages/Volunteer/VolunteerSignUp.js
@@ -2,6 +2,9 @@ import React, { Component } from "react";
 import Register from "../../components/Register";
 import { volunteerRegister } from '../../components/UserFunctions'
 
+
+
+
 class VolunteerSignUp extends Component {
     constructor() {
         super()
@@ -26,7 +29,21 @@ class VolunteerSignUp extends Component {
 
     onChange(e) {
         this.setState({ [e.target.name]: e.target.value })
+
+        if (e.target.name === "dob") {
+            var numChars = e.target.value.length;
+            if (numChars === 2 || numChars === 5) {
+                var thisVal = e.target.value;
+                thisVal += '/';
+                e.target.value = thisVal;
+
+                this.setState({ [e.target.name]: thisVal });
+                console.log(thisVal)
+                console.log("Chars: " + numChars)
+            }
+        }
     }
+
     onSubmit(e) {
         e.preventDefault()
 
@@ -48,80 +65,195 @@ class VolunteerSignUp extends Component {
         volunteerRegister(newUser, newVolunteer).then(res => {
             this.props.history.push(`/login`)
         })
+
     }
 
-
-
     render() {
+
         return (
-            <div>
-                <Register
-                    first_name={this.state.first_name}
-                    last_name={this.state.last_name}
-                    email={this.state.email}
-                    password={this.state.password}
-                    onChange={this.onChange}
-                    onSubmit={this.onSubmit}
-                />
-                <div class="container">
+            <div class="container">
+                < div >
+                    <Register
+                        first_name={this.state.first_name}
+                        last_name={this.state.last_name}
+                        email={this.state.email}
+                        password={this.state.password}
+                        onChange={this.onChange}
+                        onSubmit={this.onSubmit}
+                    />
+
+                    <form>
+                        {/* <div class="form-row">
+                            <div class="form-group col-md-6">
+                                <label for="inputEmail4">Email</label>
+                                <input type="email" class="form-control" id="inputEmail4" placeholder="Email"></input>
+                            </div>
+                            <div class="form-group col-md-6">
+                                <label for="inputPassword4">Password</label>
+                                <input type="password" class="form-control" id="inputPassword4" placeholder="Password"></input>
+                            </div>
+                        </div> */}
+                        <div className="form-row">
+                            <div className="form-group col-md-6">
+                                <label htmlFor="inputCity">City</label>
+                                <input
+                                    className="form-control"
+                                    id="inputCity"
+                                    name="city"
+                                    type="text"
+                                    value={this.state.city}
+                                    onChange={this.onChange}
+                                >
+                                </input>
+                            </div>
+                            <div className="form-group col-md-4">
+                                <label htmlFor="inputState">State</label>
+                                <select
+                                    id="inputState"
+                                    className="form-control"
+                                    name="state"
+                                    value={this.state.state}
+                                    onChange={this.onChange}>
+                                    <option selected>Choose...</option>
+                                    <option>Alabama - AL</option>
+                                    <option>Alaska - AK</option>
+                                    <option>Arizona - AZ</option>
+                                    <option>Arkansas - AR</option>
+                                    <option>California - CA</option>
+                                    <option>Colorado - CO</option>
+                                    <option>Connecticut - CT</option>
+                                    <option>Delaware - DE</option>
+                                    <option>Florida - FL</option>
+                                    <option>Georgia - GA</option>
+                                    <option>Hawaii - HI</option>
+                                    <option>Idaho - ID</option>
+                                    <option>Illinois - IL</option>
+                                    <option>Indiana - IN</option>
+                                    <option>Iowa - IA</option>
+                                    <option>Kansas - KS</option>
+                                    <option>Kentucky - KY</option>
+                                    <option>Louisiana - LA</option>
+                                    <option>Maine - ME</option>
+                                    <option>Maryland - MD</option>
+                                    <option>Massachusetts - MA</option>
+                                    <option>Michigan - MI</option>
+                                    <option>Minnesota - MN</option>
+                                    <option>Mississippi - MS</option>
+                                    <option>Missouri - MO</option>
+                                    <option>Montana - MT</option>
+                                    <option>Nebraska - NE</option>
+                                    <option>Nevada - NV</option>
+                                    <option>New Hampshire - NH</option>
+                                    <option>New Jersey - NJ</option>
+                                    <option>New Mexico - NM</option>
+                                    <option>New York - NY</option>
+                                    <option>North Carolina - NC</option>
+                                    <option>North Dakota - ND</option>
+                                    <option>Ohio - OH</option>
+                                    <option>Oklahoma - OK</option>
+                                    <option>Oregon - OR</option>
+                                    <option>Pennsylvania - PA</option>
+                                    <option>Rhode Island - RI</option>
+                                    <option>South Carolina - SC</option>
+                                    <option>South Dakota - SD</option>
+                                    <option>Tennessee - TN</option>
+                                    <option>Texas - TX</option>
+                                    <option>Utah - UT</option>
+                                    <option>Vermont - VT</option>
+                                    <option>Virginia - VA</option>
+                                    <option>Washington - WA</option>
+                                    <option>West Virginia - WV</option>
+                                    <option>Wisconsin - WI</option>
+                                    <option>Wyoming - WY</option>
+                                </select>
+                            </div>
+                            <div className="form-group col-md-2">
+                                <label htmlfor="inputZip">Zip</label>
+                                <input
+                                    type="text"
+                                    className="form-control"
+                                    id="inputZip"
+                                    name="zip"
+                                    value={this.state.zip}
+                                    onChange={this.onChange}
+                                >
+                                </input>
+                            </div>
+                        </div>
+                        <div className="form-row">
+                            <div className="form-group col-12">
+                                <label htmlFor="exampleFormControlTextarea1">Biography</label>
+                                <textarea
+                                    id="exampleFormControlTextarea1"
+                                    rows="3"
+                                    type="text"
+                                    className="form-control"
+                                    name="bio"
+                                    value={this.state.bio}
+                                    onChange={this.onChange}>
+                                </textarea>
+                            </div>
+                        </div>
 
 
-                    <div className="form-group">
-                        <label htmlFor="city">City</label>
-                        <input
-                            type="text"
-                            className="form-control"
-                            name="city"
-                            placeholder="Enter email"
-                            value={this.state.city}
-                            onChange={this.onChange}
-                        />
-                    </div>
-                    <div className="form-group">
-                        <label htmlFor="state">State</label>
-                        <input
-                            type="text"
-                            className="form-control"
-                            name="state"
-                            placeholder="Enter email"
-                            value={this.state.state}
-                            onChange={this.onChange}
-                        />
-                    </div>
-                    <div className="form-group">
-                        <label htmlFor="zip">Zipcode</label>
-                        <input
-                            type="text"
-                            className="form-control"
-                            name="zip"
-                            placeholder="Enter email"
-                            value={this.state.zip}
-                            onChange={this.onChange}
-                        />
-                    </div>
-                    <div className="form-group">
-                        <label htmlFor="dob">Date of Birth</label>
-                        <input
-                            type="text"
-                            className="form-control"
-                            name="dob"
-                            placeholder="Enter email"
-                            value={this.state.dob}
-                            onChange={this.onChange}
-                        />
-                    </div>
-                    <div className="form-group">
-                        <label htmlFor="bio">Biography</label>
-                        <input
-                            type="text"
-                            className="form-control"
-                            name="bio"
-                            placeholder="Enter email"
-                            value={this.state.bio}
-                            onChange={this.onChange}
-                        />
-                    </div>
-                    <div className="form-group">
+                        <div className="form-row">
+
+                            <div className="form-group col-md-2">
+                                <label htmlFor="exampleFormControlTextarea1">Date of Birth</label>
+                                <input
+                                    type="text"
+                                    className="form-control"
+                                    name="dob"
+                                    id="dob"
+                                    placeholder="mm/dd/yyyy"
+                                    maxlength="10"
+                                    value={this.state.dob}
+                                    onChange={this.onChange} />
+                            </div>
+
+
+                            <fieldset class="form-group ml-5">
+                                <div className="row">
+                                    <legend className="col-form-label col-sm-2 pt-0 mr-3">Gender</legend>
+                                    <div className="col-sm-10">
+                                        <div className="form-check">
+                                            <input className="form-check-input" type="radio" name="gridRadios" id="gridRadios1" value="option1" checked></input>
+                                            <label className="form-check-label" for="gridRadios1">
+                                                Male
+                                            </label>
+                                        </div>
+                                        <div className="form-check">
+                                            <input className="form-check-input" type="radio" name="gridRadios" id="gridRadios2" value="option2"></input>
+                                            <label className="form-check-label" for="gridRadios2">
+                                                Female
+                                        </label>
+                                        </div>
+                                        <div className="form-check">
+                                            <input
+                                                className="form-check-input"
+                                                type="radio"
+                                                name="gridRadios"
+                                                id="gridRadios3"
+                                                value="option3"
+                                            ></input>Other<input type="text"
+                                                className="form-control"
+                                                id="inputGender"
+                                                name="gender"
+                                                value={this.state.gender}
+                                                onChange={this.onChange}></input>
+
+                                            {/* <label className="form-check-label" for="gridRadios3">
+                                                Other
+                                            </label> */}
+                                        </div>
+                                    </div>
+                                </div>
+                            </fieldset>
+
+                        </div>
+                    </form>
+
+                    {/* <div className="form-group">
                         <label htmlFor="gender">Gender</label>
                         <input
                             type="text"
@@ -131,7 +263,7 @@ class VolunteerSignUp extends Component {
                             value={this.state.gender}
                             onChange={this.onChange}
                         />
-                    </div>
+                    </div> */}
                     <div className="form-group">
                         <label htmlFor="image">Image</label>
                         <input
@@ -150,12 +282,16 @@ class VolunteerSignUp extends Component {
                     >
                         Register!
               </button>
-                </div>
-            </div>
+                </div >
+            </div >
         )
     }
 }
 
+{/* <script
+    src="https://code.jquery.com/jquery-3.4.1.min.js"
+    integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo="
+    crossorigin="anonymous"></script> */}
 
 export default VolunteerSignUp;
 
