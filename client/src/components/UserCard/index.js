7ec6fc5ba9e944df1c76239bad1f76e1f70e7522
Lindsay <lindsay.lbm@gmail.com>
2019-12-01 11:48:27 -0600

    seeker dashboard rendring volunteers based on state

--- client/src/components/UserCard/index.js
+++ client/src/components/UserCard/index.js
@@ -7,7 +7,7 @@ function UserCard(props) {
       <h5 className="card-header">{props.first_name} {props.last_name}</h5>
       <div className="card-body">
         <h5 className="card-title">{props.city}, {props.state}</h5>
-        <p className="card-text">With supporting text below as a natural lead-in to additional content.</p>
+        <p className="card-text">{props.bio}</p>
         {/* <a href="#" className="btn btn-primary">Go somewhere</a> */}
       </div>
     </div>
