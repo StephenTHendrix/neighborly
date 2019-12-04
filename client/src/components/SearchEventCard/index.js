3484e1f94f2b2ed9a641ce995227372eb08af281
Adam Norton <adam@adamnorton.com>
2019-12-04 10:00:26 -0600

    final polish for presentation.

--- client/src/components/SearchEventCard/index.js
+++ client/src/components/SearchEventCard/index.js
@@ -6,7 +6,11 @@ function SearchEventCard(props) {
       <div class="card bg-dark text-white col-xs-12 mt-2">
         <img
           class="card-img"
-          src={"../images/" + props.image}
+          src={
+            props.image === null
+              ? "../assets/images/placeholder_event.jpg"
+              : "../images/" + props.image
+          }
           alt="Card image"
         />
         <div class="card-img-overlay p-0">
@@ -22,29 +26,64 @@ function SearchEventCard(props) {
           <p className="card-text">
             Currently Registered: {props.registered} / {props.needed}
           </p>
-          <button type="button" className="btn btn-sub" data-toggle="modal" data-target={"#" + props.id}>
-            More Information</button>
+          <button
+            type="button"
+            className="btn btn-sub"
+            data-toggle="modal"
+            data-target={"#" + props.id}
+          >
+            More Information
+          </button>
         </div>
       </div>
 
-      <div className="modal fade" id={props.id} tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+      <div
+        className="modal fade"
+        id={props.id}
+        tabindex="-1"
+        role="dialog"
+        aria-labelledby="exampleModalLabel"
+        aria-hidden="true"
+      >
         <div className="modal-dialog" role="document">
           <div className="modal-content">
             <div className="modal-header">
-              <h5 className="modal-title" id="exampleModalLabel">{props.title}</h5>
-              <button type="button" className="close text-white" data-dismiss="modal" aria-label="Close">
+              <h5 className="modal-title" id="exampleModalLabel">
+                {props.title}
+              </h5>
+              <button
+                type="button"
+                className="close text-white"
+                data-dismiss="modal"
+                aria-label="Close"
+              >
                 <span aria-hidden="true">&times;</span>
               </button>
             </div>
             <div className="modal-body">
-              <h6 className="card-subtitle mb-2 text-muted">{props.organization}</h6>
-              <p>Current attendees: {props.signup}/{props.needed}</p>
+              <h6 className="card-subtitle mb-2 text-muted">
+                {props.organization}
+              </h6>
+              <p>
+                Current attendees: {props.signup}/{props.needed}
+              </p>
               <p>{props.description}</p>
-              <p>Address: {props.street} {props.city} {props.state}</p>
-              <small className="text-muted">{props.date}  {props.time}</small>
+              <p>
+                Address: {props.street} {props.city} {props.state}
+              </p>
+              <small className="text-muted">
+                {props.date} {props.time}
+              </small>
             </div>
             <div className="modal-footer">
-              <button className="saved btn btn-sub" key={props.id} id={props.id} onClick={() => props.handleEventSignUp(props.id)}>Sign Up</button>
+              <button
+                className="saved btn btn-sub"
+                key={props.id}
+                id={props.id}
+                onClick={() => props.handleEventSignUp(props.id)}
+              >
+                Sign Up
+              </button>
             </div>
           </div>
         </div>
